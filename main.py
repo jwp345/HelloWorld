03d41b5a38f48897f7687ceb25b7a2078ef303c1
jwp345 <jwp135@naver.com>
2020-08-10 10:46:09 +0900

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def hello(name):
+    print("hello " + name)
+    print("hello " + name)
+    print("hello " + name)
+    if True:
+        print('ok')
+        print('ok')
+
+
+hello("kickscar")
